ce2803ecb7c695c9871a0b19f615e5b4746252d1
vidhan-arora <vidhanarora1997@gmail.com>
2019-04-19 14:15:49 +0530

    my first commit

--- regration in ml/movie_nlp.py
+++ regration in ml/movie_nlp.py
@@ -15,7 +15,9 @@ Perform sentiment analysis on the text reviews to determine whether its positive
  
 import pandas as pd
 import numpy as np
+import matpoltlib.pyplot as plt
 dataset=pd.read_csv("movie.csv")
+
 import re
 import nltk
 nltk.download("stopwords")
@@ -49,10 +51,59 @@ labels_pred = classifier.predict(features_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(labels_test, labels_pred)
+classifier.score(features_test,labels_test)
+#==================================================================================
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+dataset=pd.read_csv("movie.csv")
+from sklearn.naive_bayes import GaussianNB,BernoulliNB,MultinomialNB
+import re
+import nltk
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus=[]
+for i in range(0,2000):
+    text = re.sub('[^a-zA-Z]', ' ', dataset['text'][i])
+    text = text.lower()
+    text = text.split()
+    text = [word for word in text if  word not in set(stopwords.words('english'))]
+    ps=PorterStemmer()
+    text=[ps.stem(word) for word in text]
+    text=' '.join(text)
+    corpus.append(text)
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+features = cv.fit_transform(corpus).toarray()
+dataset["class"]=np.where(dataset["class"]=="Pos",0,1)
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, dataset["class"], test_size = 0.20, random_state = 0)
 
+gnb=GaussianNB()
 
+gnb.fit(features_train,labels_train)
+        
+label_pred=gnb.predict(features_test)
+from sklearn.metrics import confusion_matrix
+cm_gnb=confusion_matrix(labels_test,label_pred)
+gnb.score(features_test,labels_test)
+#===============================================================================
+bnb=BernoulliNB()
 
- 
- 
- 
- 
+bnb.fit(features_train,labels_train)
+        
+label_pred=bnb.predict(features_test)
+from sklearn.metrics import confusion_matrix
+cm_bnb=confusion_matrix(labels_test,label_pred)
+bnb.score(features_test,labels_test)
+ #===============================================================================
+mnb=MultinomialNB()
+
+mnb.fit(features_train,labels_train)
+        
+label_pred=mnb.predict(features_test)
+from sklearn.metrics import confusion_matrix
+cm_mnb=confusion_matrix(labels_test,label_pred)
+mnb.score(features_test,labels_test) 
+#for this question multinomial is  best.
